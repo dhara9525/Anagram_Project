d6675c08c03d823beceb1342b22311ca53203435
Dhara Tamhane <dhara9525@gmail.com>
2018-02-11 17:28:37 -0800

    Anagram Project
    1. Project Instructions
    2. Source code
    3. Output Screenshots

---
+++ Project_1.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+
+public class Program
+{
+	public static void Main()
+	{
+		Console.WriteLine("Enter input as space seperaed values: ");
+		String inputString = Console.ReadLine();
+		inputString = inputString.Trim();
+		if (String.IsNullOrEmpty(inputString) == true || inputString == " ")
+		{
+			Console.WriteLine("Input string is not valid");
+			return;
+		}
+
+		Char delimiter = ' ';
+		String[] substrings = inputString.Split(delimiter);
+		List<String> stringList = new List<String>();
+		foreach (String st in substrings)
+		{
+			stringList.Add(st);
+		}
+
+		ismatch(stringList);
+	}
+
+	public static void ismatch(List<String> stringList)
+	{
+		int length = stringList.Count;
+		List<String> stringList1 = new List<String>();
+		int count = 0;
+		for (int i = 0; i < length; i++)
+		{
+			for (int j = i + 1; j < length; j++)
+			{
+				char[] a = stringList[i].ToLower().ToCharArray();
+				char[] b = stringList[j].ToLower().ToCharArray();
+				Array.Sort(a);
+				Array.Sort(b);
+				if (new String(a).Equals(new String(b)))
+				{
+					stringList1.Add(stringList[i]);
+					stringList1.Add(stringList[j]);
+					stringList.Remove(stringList[j]);
+					length = length - 1;
+				}
+			}
+		}
+
+		foreach (String resultString in stringList1)
+		{
+			Console.WriteLine("\n " + resultString);
+			count++;
+		}
+
+		if (count == 0)
+		{
+			Console.WriteLine("In the given string anagram words don't exist");
+			return;
+		}
+	}
+}
